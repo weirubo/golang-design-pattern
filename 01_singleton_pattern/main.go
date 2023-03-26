a818fcc1e51c6bf778077ae2640d96d4998961db
weirubo <gopher@88.com>
2023-03-26 17:40:44 +0800

    singleton pattern

--- 01_singleton_pattern/main.go
+++ 01_singleton_pattern/main.go
@@ -1,12 +1,12 @@
 package main
 
-import v6 "golang-design-pattern/01_singlegon_pattern/v6"
+import v1 "golang-design-pattern/01_singleton_pattern/v1"
 
 func main() {
-	//v1.GetInstance()
+	v1.GetInstance()
 	//v2.GetInstance()
 	//v3.GetInstance()
 	//v4.GetInstance()
 	//v5.GetInstance()
-	v6.GetInstance()
+	//v6.GetInstance()
 }
